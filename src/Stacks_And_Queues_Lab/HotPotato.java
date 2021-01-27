0ad6834420d957c346706ccba1c6e7438f900b8e
Lora <cruzeiro@abv.bg>
2021-01-27 22:48:22 +0200

    Initial Commit

---
+++ src/Stacks_And_Queues_Lab/HotPotato.java
@@ -0,0 +1,30 @@
+package Stacks_And_Queues_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class HotPotato {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = scanner.nextLine().split("\\s+");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<String> children = new ArrayDeque<>();
+        Collections.addAll(children, names);
+
+        while (children.size() > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                String currentChild = children.poll();
+                children.offer(currentChild);
+            }
+
+            String child = children.poll();
+            System.out.println("Removed " + child);
+        }
+        System.out.println("Last is " + children.peek());
+    }
+}
