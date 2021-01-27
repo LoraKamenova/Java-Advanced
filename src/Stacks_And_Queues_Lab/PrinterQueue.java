b5625e9a26588235078f9e17e9c6d4a9d40506ee
Lora <cruzeiro@abv.bg>
2021-01-27 22:52:10 +0200

    Initial Commit

---
+++ src/Stacks_And_Queues_Lab/PrinterQueue.java
@@ -0,0 +1,31 @@
+package Stacks_And_Queues_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
+
+        while (!line.equals("print")) {
+            if (line.equals("cancel")) {
+                if(printerQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled " + printerQueue.poll());
+                }
+            } else {
+                printerQueue.offer(line);
+            }
+
+            line = scanner.nextLine();
+        }
+
+        while (!printerQueue.isEmpty()) {
+            System.out.println(printerQueue.poll());
+        }
+    }
+}
