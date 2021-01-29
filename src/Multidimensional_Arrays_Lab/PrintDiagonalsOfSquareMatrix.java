51434db4580e7f5fb9ffcf1004664e881e983a6b
Lora <cruzeiro@abv.bg>
2021-01-29 21:04:18 +0200

    Initial Commit

---
+++ src/Multidimensional_Arrays_Lab/PrintDiagonalsOfSquareMatrix.java
@@ -0,0 +1,32 @@
+package Multidimensional_Arrays_Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintDiagonalsOfSquareMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            int[] rowData = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            matrix[i] = rowData;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(matrix[i][i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(matrix[n - 1 - i][i] + " ");
+        }
+        System.out.println();
+    }
+}
