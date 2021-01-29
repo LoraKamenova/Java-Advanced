eec33d4fc47cf99bc519439c9aa89a64ae881856
Lora <cruzeiro@abv.bg>
2021-01-29 20:59:47 +0200

    Initial Commit

---
+++ src/Multidimensional_Arrays_Lab/CompareMatrices.java
@@ -0,0 +1,50 @@
+package Multidimensional_Arrays_Lab;
+
+import java.util.Scanner;
+
+public class CompareMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] rowsCols = scanner.nextLine().split("\\s+");
+        int rows = Integer.parseInt(rowsCols[0]);
+        int cols = Integer.parseInt(rowsCols[1]);
+
+        int [][] firstMatrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            String [] colData = scanner.nextLine().split("\\s+");
+            for (int col = 0; col < cols ; col++) {
+                firstMatrix[row][col] = Integer.parseInt(colData[col]);
+            }
+        }
+
+        rowsCols = scanner.nextLine().split("\\s+");
+        rows = Integer.parseInt(rowsCols[0]);
+        cols = Integer.parseInt(rowsCols[1]);
+
+        int [][] secondMatrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            String [] colData = scanner.nextLine().split("\\s+");
+            for (int col = 0; col < cols ; col++) {
+                secondMatrix[row][col] = Integer.parseInt(colData[col]);
+            }
+        }
+
+        if (firstMatrix[0].length != secondMatrix[0].length ||
+                firstMatrix.length != secondMatrix.length) {
+            System.out.println("not equal");
+            return;
+        }
+
+        for (int row = 0; row < firstMatrix.length; row++) {
+            for (int col = 0; col < secondMatrix[0].length; col++) {
+                if(firstMatrix[row][col] != secondMatrix[row][col]) {
+                    System.out.println("not equal");
+                    return;
+                }
+            }
+        }
+        System.out.println("equal");
+    }
+}
