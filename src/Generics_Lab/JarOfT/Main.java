e2bf6f25c3611080f31e440309ddc739ab04d778
Lora <cruzeiro@abv.bg>
2021-02-04 18:07:17 +0200

    Initial Commit

---
+++ src/Generics_Lab/JarOfT/Main.java
@@ -0,0 +1,12 @@
+package Generics_Lab.JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<String> jar = new Jar<>();
+
+        jar.add("first");
+        jar.add("second");
+
+        System.out.println( jar.remove());
+    }
+}
